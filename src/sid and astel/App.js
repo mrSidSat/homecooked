945a8ac77e8b85c0264a52b0a18b9c0cc34c4a1c
Strikers47 <sidsat477501@gmail.com>
2021-01-13 10:42:53 +0530

    seperate files for groups in src folder

--- src/sid and astel/App.js
+++ src/sid and astel/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+console.log(Trial());
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
